60662fb8ccf858759257af70180a74eb9fbe6857
shereen makram <140805315+shereenm1521@users.noreply.github.com>
2023-09-22 19:56:50 +0300

    Create test.groovy

---
+++ vars/test.groovy
@@ -0,0 +1,34 @@
+#!/usr/bin/env groovy
+
+def call(){
+   
+
+   pipeline {
+    
+    agent {
+      label  'slave'
+    }
+
+    stages {
+        
+        stage('install nginx') {
+            steps {
+               sh 'sudo apt install nginx -y'
+            }
+        }
+        stage('start nginx') {
+            steps {
+                sh "sudo systemctl nginx enable"
+                 sh "sudo systemctl nginx start"
+            }    
+        }
+       stage('test nginx') {
+            steps {
+                sh "curl localhost "
+            }    
+        }
+       
+    }
+  
+}
+}
